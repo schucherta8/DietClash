f1fac287e09baa4fd0528c5303863f30e3b033b6
thomasdiaz1 <46580678+thomasdiaz1@users.noreply.github.com>
2020-04-06 11:28:08 -0400

    FoodServings Table Added for GetStarted

--- DietClash/app/src/main/java/com/diet/dietclash/FoodDB/FoodDBHelper.java
+++ DietClash/app/src/main/java/com/diet/dietclash/FoodDB/FoodDBHelper.java
@@ -7,23 +7,69 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class FoodDBHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "Food.db";
+
+    /**
+     * CREATE_STRING Constant:
+     * FoodEntryContract Initialization
+     * CREATE TABLE food{
+     *     id Int,
+     *     category TEXT,
+     *     amount Int,
+     *     date TEXT;
+     * }
+     */
     private static final String CREATE_STRING = "CREATE TABLE "+ FoodEntryContract.FoodEntry.TABLE_NAME +
             " ("+ FoodEntryContract.FoodEntry._ID + " INTEGER PRIMARY KEY," +
             FoodEntryContract.FoodEntry.COLUMN_NAME_CATEGORY+" TEXT," +
             FoodEntryContract.FoodEntry.COLUMN_NAME_AMOUNT+" INTEGER," +
             FoodEntryContract.FoodEntry.COLUMN_NAME_DATE+" TEXT)";
+
+
+    /**
+     * CREATE_STRING_SERVINGS Constant:
+     * FoodServingsContract Initialization
+     * CREATE TABLE servings{
+     *     id Int,
+     *     category TEXT,
+     *     amount Int;
+     * }
+     */
+    private static final String CREATE_STRING_SERVINGS = "CREATE TABLE "+ FoodServingsContract.FoodServings.TABLE_NAME +
+            " ("+ FoodEntryContract.FoodEntry._ID + " INTEGER PRIMARY KEY," +
+            FoodEntryContract.FoodEntry.COLUMN_NAME_CATEGORY+" TEXT," +
+            FoodEntryContract.FoodEntry.COLUMN_NAME_AMOUNT+" INTEGER)";
+
+    /**
+     * DROP_STRING Constant:
+     * FoodEntryContract Drop Table
+     * DROP TABLE IF EXISTS food;
+     */
     private static final String DROP_STRING = "DROP TABLE IF EXISTS "+ FoodEntryContract.FoodEntry.TABLE_NAME;
+
+    /**
+     * DROP_STRING_SERVINGS Constant:
+     * FoodServingsContraction Drop Table
+     * DROP TABLE IF EXISTS servings;
+     */
+    private static final String DROP_STRING_SERVINGS = "DROP TABLE IF EXISTS "+ FoodServingsContract.FoodServings.TABLE_NAME;
 
     public FoodDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //Create the servings table respecting referential integrity
+        db.execSQL(CREATE_STRING_SERVINGS);
+        //Create the food table
         db.execSQL(CREATE_STRING);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Drop the servings table respecting referential integrity
+        db.execSQL(DROP_STRING_SERVINGS);
+        //Drop the food table
         db.execSQL(DROP_STRING);
         onCreate(db);
     }
---
+++ DietClash/app/src/main/java/com/diet/dietclash/FoodDB/FoodServingsContract.java
@@ -0,0 +1,14 @@
+package com.diet.dietclash.FoodDB;
+
+import android.provider.BaseColumns;
+
+public final class FoodServingsContract {
+    private FoodServingsContract(){}
+
+    /* Inner class that defines the table contents */
+    public static class FoodServings implements BaseColumns {
+        public static final String TABLE_NAME = "servings";
+        public static final String COLUMN_NAME_CATEGORY = "category";
+        public static final String COLUMN_NAME_AMOUNT = "amount";
+    }
+}
